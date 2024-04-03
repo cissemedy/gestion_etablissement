67aa0c1d71d57abd4f23ab894f1902142aca9114
fousseynou camara <camarafousseynou160@gmail.com>
2024-04-02 20:21:01 -1200

    la page de connexion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -179,9 +179,9 @@
 
             
             <li class="nav-item dropdown no-arrow">
-                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                <a class="nav-link dropdown-toggle" href="register" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Se Connecter</span>
                     <img class="img-profile rounded-circle"
                         src="img/undraw_profile.svg">
                 </a>
