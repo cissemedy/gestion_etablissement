149ba1718093628188956de0cf733016569c5db2
fousseynou camara <camarafousseynou160@gmail.com>
2024-04-05 10:55:10 -1200

    recuperation du projet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -181,7 +181,7 @@
             <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="register" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Sam SALL</span>
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Se Connecter</span>
                     <img class="img-profile rounded-circle"
                         src="img/undraw_profile.svg">
                 </a>
